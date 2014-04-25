6056e7f6fd1de6a25551e734beaa341183425540
SeedPaths-User <lessdismissive@email.com>
2014-04-25 13:48:19 -0600

    here you go
    Ninja turtles

---
+++ ObjectPractice/Turtle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class Turtle
+    {
+        public string shell { get; set; }
+        public string body { get; set; }
+
+        Turtle MutantTurtle = new Turtle();
+        Turtle SeaTurtle = new Turtle();
+
+        public void ninjaTurtleMoves{
+
+
+    }
+}
